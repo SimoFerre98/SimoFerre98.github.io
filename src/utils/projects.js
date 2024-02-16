51ea10bd04236879207ef51f19d0bdce1f899e73
Simone Ferrero <63207675+SimoFerre98@users.noreply.github.com>
2024-02-16 17:39:03 +0100

    Aggiornamento post tesi

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -5,56 +5,25 @@ import blog from "../assets/images/blog.webp";
 import docAid from "../assets/images/docAid.webp";
 const data = [
     {
-        name: "DocAid",
-        type: "Website",
+        name: "BlockChange",
+        type: "Cross-Platform App",
         url: "https://doc-aid.vercel.app/",
-        github: 'https://github.com/DevRohit06/DocAid',
+        github: 'https://github.com/SimoFerre98/BlockChange',
         image: docAid,
         slug: "docaid",
-        description: "Website template for Health related businesses",
-        tech: ['HTML', 'Javascript', 'CSS', 'Tailwind css', 'ReactJS']
+        description: "Thesis project for the degree in Computer Engineering. The project is a decentralized application that allows to buy cripto through the blockchain using testnet.",
+        tech: ['Dart', 'JavaScript', 'Kotlin', 'Solidity', 'Swift','HTML']
     },
     {
-        name: "DevBlogs",
-        type: "WebApp",
-        url: "https://www.devblogs.xyz/",
-        github: "https://github.com/DevRohit06/blog",
+        name: "LeoFunLab",
+        type: "Website",
+        url: "https://www.leofunlab.com/",
+        github: "",
         image: blog,
         slug: "devblogs",
         description: "This is a Blogging website made for technology lovers.",
         tech: ['HTML', 'Javascript', 'Css', 'Tailwind css', 'Astro', 'Markdown']
-    },
-    {
-        name: "Ras Production",
-        type: "Website",
-        url: "https://rasproduction.vercel.app/",
-        github: "https://github.com/DevRohit06/videography-website",
-        image: ras,
-        slug: "ras",
-        description: "This is an Portfolio website for videography businesses made using HTML, Javascript, Css and Tailwind css it practices modern ui with dark theme ",
-        tech: ['HTML', 'Javascript', 'Css', 'Tailwind css']
-    },
-    {
-        name: "Joke Generator",
-        type: "WebApp",
-        url: "https://jokes-generator-with-api.vercel.app/",
-        github: "https://github.com/DevRohit06/Jokes-Generator-With-API",
-        image: joke,
-        slug: "joke",
-        description: "The website which generates joke with multi topics such as anime, programming, etc.",
-        tech: ['React', 'Javascript', 'Css', 'Tailwind css', 'express', 'nodejs']
-    },
-
-    {
-        name: "Lofi Beats",
-        type: "Website",
-        url: "https://lofibeats-3oo4q8gbg-lofi.vercel.app/",
-        github: "https://github.com/Lofi-Beats/lofibeats",
-        image: lofi,
-        slug: "lofibeats",
-        description: "Website that was built for Discord music bot named lofi beats built using plain html javascript and tailwind css",
-        tech: ['HTML', 'Javascript', 'Css', 'Tailwind css']
-    },
+    }
 ]
 
 export function getData(){
