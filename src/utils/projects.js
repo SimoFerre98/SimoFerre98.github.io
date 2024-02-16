50067bf00a55618702d595bd2f8df19bb64dbf8d
Simone Ferrero <63207675+SimoFerre98@users.noreply.github.com>
2024-02-16 17:43:58 +0100

    Aggiunta immagine progetto

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,14 +2,14 @@ import ras from "../assets/images/ras.webp";
 import joke from "../assets/images/joke.webp";
 import lofi from "../assets/images/lofi.webp";
 import blog from "../assets/images/blog.webp";
-import docAid from "../assets/images/docAid.webp";
+import BlockChange from "../assets/images/BlockChange.png";
 const data = [
     {
         name: "BlockChange",
         type: "Cross-Platform App",
         url: "https://doc-aid.vercel.app/",
         github: 'https://github.com/SimoFerre98/BlockChange',
-        image: docAid,
+        image: BlockChange,
         slug: "docaid",
         description: "Thesis project for the degree in Computer Engineering. The project is a decentralized application that allows to buy cripto through the blockchain using testnet.",
         tech: ['Dart', 'JavaScript', 'Kotlin', 'Solidity', 'Swift','HTML']
